045fa87ecad5dfc995cbbe3f7c0117581cb961d5
Arkadiy Katz <arkadiyk@mail.afeka.ac.il>
2019-12-06 17:33:49 +0200

    Added project data to git

---
+++ src/java/waf/src/main/java/org/afeka/project/WAFModule.java
@@ -0,0 +1,10 @@
+package org.afeka.project;
+
+import com.google.inject.AbstractModule;
+
+public class WAFModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
