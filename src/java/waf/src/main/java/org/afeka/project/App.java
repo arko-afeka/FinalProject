89bb6b8a9328667bbae1c5393930e0f3a3113ec5
Arkadiy Katz <arkadiyk@mail.afeka.ac.il>
2019-12-21 21:13:18 +0200

    Updated handling wait for signal

--- src/java/waf/src/main/java/org/afeka/project/App.java
+++ src/java/waf/src/main/java/org/afeka/project/App.java
@@ -4,27 +4,24 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.afeka.project.server.WAFServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main(String[] args )
-    {
-        Injector injector = Guice.createInjector(new WAFModule());
-        WAFBootstrap boostrap = injector.getInstance(WAFBootstrap.class);
+/** Hello world! */
+public class App {
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
 
-        try {
-            WAFServer server = boostrap.getServer(8080);
-            server.start();
-            Thread.currentThread().wait();
-            server.stop();
-        } catch (IOException | InterruptedException ex) {
-            ex.printStackTrace();
-        }
+  public static void main(String[] args) {
+    Injector injector = Guice.createInjector(new WAFModule());
+    WAFBootstrap boostrap = injector.getInstance(WAFBootstrap.class);
+
+    try {
+      WAFServer server = boostrap.getServer(8080);
+      server.start();
+    } catch (IOException ex) {
+      logger.error("Problem in startup", ex);
     }
+  }
 }
